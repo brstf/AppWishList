07d2e8e28ed92a46d5c44c8999d1abd02377f735
Brian Stauffer <brianrstauffer@gmail.com>
2012-06-10 14:36:17 -0400

    Slightly modified a few layouts.

--- gen/com/brstf/appwishlist/R.java
+++ gen/com/brstf/appwishlist/R.java
@@ -16,11 +16,14 @@ public final class R {
     public static final class id {
         public static final int appicon=0x7f050001;
         public static final int appname=0x7f050002;
-        public static final int btndel=0x7f050005;
-        public static final int btnrev=0x7f050006;
+        public static final int btndel=0x7f050007;
+        public static final int btnrev=0x7f050008;
+        public static final int btnshr=0x7f050006;
+        public static final int btntag=0x7f050005;
         public static final int fragment_container=0x7f050000;
         public static final int price=0x7f050003;
         public static final int sale_price=0x7f050004;
+        public static final int shiftpos=0x7f050009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
