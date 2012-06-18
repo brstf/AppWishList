21ed33ca189086a188a86e0251eab14224006657
Brian Stauffer <brianrstauffer@gmail.com>
2012-06-18 17:09:14 -0400

    Moved SquareButton from appwishlist to appwishlist.widgets, added new widget: SquareImageView

---
+++ src/com/brstf/appwishlist/widgets/SquareButton.java
@@ -0,0 +1,27 @@
+package com.brstf.appwishlist.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class SquareButton extends Button {
+
+	public SquareButton(Context context) {
+	    super(context);
+	}
+	
+	public SquareButton(Context context, AttributeSet attrs) {
+	    super(context, attrs);
+	}
+	
+	 @Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+	
+	    int width = MeasureSpec.getSize(widthMeasureSpec);
+	
+	    super.onMeasure(
+	            MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY),
+	            MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY)
+	    );
+	}
+}
