c02fc444dd219695181448a916a3b45baeeea4a3
Brian Stauffer <brianrstauffer@gmail.com>
2012-08-15 18:44:24 -0400

    Added NetworkUtils

--- src/com/brstf/wishlist/util/NetworkUtils.java
+++ src/com/brstf/wishlist/util/NetworkUtils.java
@@ -8,6 +8,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 public class NetworkUtils {
@@ -34,5 +37,35 @@ public class NetworkUtils {
 			Log.d("WL", "Error");
 			return "Error";
 		}
+	}
+
+	/**
+	 * Public function to determine whether or not an internet connection is
+	 * available.
+	 * 
+	 * @param context Application context to test network connectivity
+	 * @return True if the internet is reachable, false otherwise
+	 */
+	public static boolean isNetworkAvailable(Context context) {
+		ConnectivityManager connectivityManager = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager
+				.getActiveNetworkInfo();
+		return activeNetworkInfo != null;
+	}
+
+	/**
+	 * Public function to determine whether or not wifi is connected.
+	 * 
+	 * @param context Application context to test network connectivity
+	 * @return True if WiFi is reachable, false otherwise
+	 */
+	public static boolean isWifiAvailable(Context context) {
+		ConnectivityManager connectivityManager = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager
+				.getActiveNetworkInfo();
+		return activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI
+				&& activeNetworkInfo.isConnected();
 	}
 }
